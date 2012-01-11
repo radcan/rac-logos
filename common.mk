836dac0db73869bd8de94465a16915ff90df475f
Tyler Tidman <tyler.tidman@draak.ca>
2012-01-11 13:28:24 -0500

    Added pdf output to the makefile

--- common.mk
+++ common.mk
@@ -3,9 +3,10 @@ SHELL = /bin/bash
 SVG = $(wildcard *.svg)
 PNG = $(SVG:.svg=.png)
 THUMB = $(SVG:.svg=.thumb.png)
+PDF = $(SVG:.svg=.pdf)
 JPG = $(SVG:.svg=.jpg)
 
-GENERATED_FILES = $(PNG) $(THUMB) $(JPG)
+GENERATED_FILES = $(PNG) $(THUMB) $(PDF) $(JPG)
 
 .PHONY : all
 all : $(GENERATED_FILES)
@@ -15,6 +16,9 @@ all : $(GENERATED_FILES)
 
 %.thumb.png : %.png
 	@convert -size 125x125 -resize 125x125 $< $@
+
+%.pdf : %.svg
+	@inkscape -A $@ $<
 
 %.jpg : %.png
 	@convert $< $@
